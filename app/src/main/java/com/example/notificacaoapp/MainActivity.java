8b38a59a54dc9199e1f292de05427eff24fcdffe
Wemerson7 <wemerson_damasceno@yahoo.com>
2020-10-08 19:19:47 -0300

    ...

--- app/src/main/java/com/example/notificacaoapp/MainActivity.java
+++ app/src/main/java/com/example/notificacaoapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.notificacaoapp;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -40,13 +45,8 @@ public class MainActivity extends AppCompatActivity {
         btnNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        displayNotification();
-                    }
-                }, 10000);
-}
+                displayNotification();
+            }
         });
 
 
@@ -55,14 +55,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayNotification() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+        /*NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_action_name)
                 .setContentTitle("Titulo da notification")
                 .setContentText("Mensagem da notification")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
 
         NotificationManagerCompat mNotificationMG = NotificationManagerCompat.from(this);
-        mNotificationMG.notify(1, builder.build());
+        mNotificationMG.notify(1, builder.build());*/
+
+
+        Intent intent = new Intent(this, NotificationActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),0,intent,0);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        String notificationChanelId = "my_chanel_id_01";
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChanel = new NotificationChannel(notificationChanelId, "MyNotifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChanel.setDescription("Channel description");
+            notificationChanel.enableLights(true);
+            notificationChanel.setLightColor(Color.RED);
+
+            notificationManager.createNotificationChannel(notificationChanel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, notificationChanelId);
+
+        builder.setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Titulo da notificacao")
+                .setContentText("Texto da notificacao")
+                .setContentIntent(pendingIntent);
+
+        notificationManager.notify(1,builder.build());
+
 
     }
 }
